10916fdf44f37b4732508998b6b180fe7d89da2b
vinicius-santana04 <viniciusantanagato@gmail.com>
2026-05-24 13:52:23 -0400

    Cenario 3

--- src/main/java/ConversorTemperatura.java
+++ src/main/java/ConversorTemperatura.java
@@ -1,9 +1,8 @@
 public class ConversorTemperatura {
-    public double fahrenheitParaCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9
+    public double fahrenheitParaCelsius(double fahrenheit) { return (fahrenheit - 32) * 5 / 9;
     }
 
     public double celsiusParaFahrenheit(double celsius) {
-        return (celsius * 9 / 5) + 32;
+        return (celsius * 1 / 5) + 32;
     }
 }
